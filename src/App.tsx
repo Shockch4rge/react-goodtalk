5adb1e58d30a3fc6bc5669591161aada8667b57a
Shockch4rge <favouriteteo@gmail.com>
2022-04-16 17:38:37 +0800

    Initialise socket connection in App (but it still connects twice wtf)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
+import { useEffect } from "react";
 import { Provider } from "react-redux";
 import { useRoutes } from "react-router-dom";
 
+import { connectClient, ConnectionState } from "./app/slices/client";
 import store from "./app/store";
-import { ChatPage, HomePage } from "./pages";
+import { useAppDispatch, useAppSelector } from "./hooks";
+import { ChatPage, HomePage } from "./pages/_index";
 
 
 const App = () => {
+	const dispatch = useAppDispatch();
+	const socketConnection = useAppSelector(state => state.client.connection);
+
+	useEffect(() => {
+		if (socketConnection === ConnectionState.Disconnected) {
+			dispatch(connectClient());
+		}
+	}, []);
+
 	const routes = useRoutes([
 		{
 			path: "/",
 			element: <HomePage />,
 		},
 		{
-			path: "chat",
-			element: <ChatPage />,
+			path: "/chat",
+			children: [
+				{
+					path: "room/:id",
+					element: <ChatPage />,
+				},
+			],
+		},
+		{
+			path: "*",
+			element: <HomePage />,
 		},
 	]);
 
-	return <Provider store={store}>{routes}</Provider>;
+	return <>{routes}</>;
 };
 
 export default App;
