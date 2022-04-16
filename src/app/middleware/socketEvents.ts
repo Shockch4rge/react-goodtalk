1605b2521699c1259a2df3194ad381cba7523fcc
Shockch4rge <favouriteteo@gmail.com>
2022-04-16 22:54:41 +0800

    More consistent event names

--- src/app/middleware/socketEvents.ts
+++ src/app/middleware/socketEvents.ts
@@ -1,11 +1,12 @@
 import { io, Socket } from "socket.io-client";
 
-import { Middleware } from "@reduxjs/toolkit";
+import { Action, Middleware } from "@reduxjs/toolkit";
 
 import { ClientEvents, ServerEvents } from "../../typings/events";
-import { messageDeleted, messageReceived } from "../slices/chat";
+import { receivedAllRoomMessages, receivedAllRooms } from "../slices/chat";
 import {
-    clientConnected, clientConnecting, ConnectionState, deleteMessage, sendMessage
+    clientConnected, connectClient, ConnectionState, createdMessage, createMessage, deletedMessage,
+    deleteMessage, receivedClientData, requestAllRoomMessages, requestAllRooms, requestClientData
 } from "../slices/client";
 import { AppState } from "../store";
 
@@ -13,31 +14,59 @@ import { AppState } from "../store";
 const socketMiddleware: Middleware<{}, AppState> = store => {
 	let socket: Socket<ServerEvents, ClientEvents>;
 
-	return next => action => {
+	return next => (action: Action<unknown>) => {
 		const isConnected = socket && store.getState().client.connection === ConnectionState.Connected;
 
-		if (clientConnecting.match(action)) {
-			socket = io("http://localhost:4000");
+		if (connectClient.match(action)) {
+			socket = io("http://localhost:4000", {
+				withCredentials: true,
+			});
 
 			socket.on("connect", () => {
 				store.dispatch(clientConnected());
+				const id = "cl21tl1dj00073susq35zeits";
+				store.dispatch(requestClientData(id));
 			});
 
-			socket.on("messageReceived", message => {
-				store.dispatch(messageReceived(message));
+			socket.on("createdMessage", message => {
+				store.dispatch(createdMessage(message));
 			});
 
-			socket.on("messageDeleted", message => {
-				store.dispatch(messageDeleted(message));
+			socket.on("createdMessage", message => {
+				store.dispatch(deletedMessage(message));
+			});
+
+			socket.on("sentAllRoomMessages", messages => {
+				store.dispatch(receivedAllRoomMessages(messages));
+			});
+
+			socket.on("sentAllRooms", rooms => {
+				store.dispatch(receivedAllRooms(rooms));
+			});
+
+			socket.on("sentClientData", client => {
+				store.dispatch(receivedClientData(client));
 			});
 		}
 
-		if (sendMessage.match(action) && isConnected) {
-			socket.emit("messageCreate", action.payload);
+		if (createMessage.match(action) && isConnected) {
+			socket.emit("createMessage", action.payload);
 		}
 
 		if (deleteMessage.match(action) && isConnected) {
-			socket.emit("messageDelete", action.payload);
+			socket.emit("deleteMessage", action.payload);
+		}
+
+		if (requestAllRooms.match(action) && isConnected) {
+			socket.emit("requestAllRooms");
+		}
+
+		if (requestAllRoomMessages.match(action) && isConnected) {
+			socket.emit("requestAllRoomMessages", action.payload);
+		}
+
+		if (requestClientData.match(action) && isConnected) {
+			socket.emit("requestClientData", action.payload);
 		}
 
 		next(action);
