bf3ab35719d619ed78c9c2db62b722f8a682168f
Shockch4rge <favouriteteo@gmail.com>
2022-04-18 23:39:18 +0800

    Additional styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,41 @@
+const plugin = require("tailwindcss/plugin");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
 			colors: {
 				primary: "",
-				"btn-primary": "",
 			},
 		},
-
 		container: {
 			center: true,
 		},
 	},
-	plugins: [require("@tailwindcss/forms")],
+	plugins: [
+		require("@tailwindcss/forms"),
+		plugin(({ addComponents }) => {
+			addComponents({
+				".btn-primary": {
+					padding: ".5rem 1rem",
+					borderRadius: ".5rem",
+					backgroundImage: "var(--primary-gradient)",
+
+					"&:hover": {
+						backgroundImage: "var(--primary-gradient-hover)",
+					},
+				},
+
+				".btn-secondary": {
+					padding: ".5rem 1rem",
+					borderRadius: ".5rem",
+					backgroundColor: "#FFFFFF",
+					"&:hover": {
+						backgroundColor: colors.gray["100"],
+					},
+				},
+			});
+		}),
+	],
 };
